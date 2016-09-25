617db3b3383baf59daab3a5cb4f95701bb0959d2
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 01:54:20 +0300

    Initial commit.

---
+++ src/main/java/sp/data/services/interfaces/OrderStatusService.java
@@ -0,0 +1,10 @@
+package sp.data.services.interfaces;
+
+import sp.data.entities.OrderStatus;
+import sp.data.services.generic.GenericService;
+
+public interface OrderStatusService extends GenericService<OrderStatus, Integer> {
+
+
+	
+}

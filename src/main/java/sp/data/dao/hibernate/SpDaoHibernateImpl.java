b787480f23e61b31295115d421a18c58a428da64
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 01:39:30 +0300

    Added getByIdWithAllChildren() method.

--- src/main/java/sp/data/dao/hibernate/SpDaoHibernateImpl.java
+++ src/main/java/sp/data/dao/hibernate/SpDaoHibernateImpl.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
@@ -14,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import sp.data.dao.generic.GenericDaoHibernateImpl;
 import sp.data.dao.interfaces.SpDao;
+import sp.data.entities.Order;
 import sp.data.entities.Sp;
 
 @Repository("SpDaoHibernateImpl")
@@ -33,6 +32,7 @@ public class SpDaoHibernateImpl extends GenericDaoHibernateImpl<Sp, Integer> imp
 		CriteriaBuilder cb = currentSession().getCriteriaBuilder();
 		CriteriaQuery<Sp> q = cb.createQuery(Sp.class);
 		Root<Sp> root = q.from(Sp.class);
+		root.fetch("status");
 
 		q.select(root);	
 		q.orderBy(cb.desc(root.get("id")));	
@@ -43,20 +43,36 @@ public class SpDaoHibernateImpl extends GenericDaoHibernateImpl<Sp, Integer> imp
 	}
 
 	@Override
-	public Sp getByNumber(int number) {
+	public Sp getByIdLazy(int id) {
 		
 		CriteriaBuilder cb = currentSession().getCriteriaBuilder();
 		CriteriaQuery<Sp> q = cb.createQuery(Sp.class);
 		Root<Sp> root = q.from(Sp.class);
 
 		q.select(root);	
-		q.where(cb.equal(root.<String>get("number"), number));
+		q.where(cb.equal(root.<String>get("id"), id));
 		
 		TypedQuery<Sp> tq = currentSession().createQuery(q);
 		
 		return tq.getResultList().get(0);
 	}
 
+	@Override
+	public Sp getByIdWithAllChildren(int id) {
+		CriteriaBuilder cb = currentSession().getCriteriaBuilder();
+		CriteriaQuery<Sp> q = cb.createQuery(Sp.class);
+		Root<Sp> root = q.from(Sp.class);
 
+		Fetch<Sp, Order> spOrdedFetch = root.fetch("orders");
+		spOrdedFetch.fetch("orderPositions", JoinType.LEFT).fetch("product", JoinType.LEFT);
+		spOrdedFetch.fetch("client");
+		root.fetch("status");
 
+		q.select(root);
+		q.where(cb.equal(root.<String>get("id"), id));
+
+		TypedQuery<Sp> tq = currentSession().createQuery(q);
+
+		return tq.getResultList().get(0);
+	}
 }

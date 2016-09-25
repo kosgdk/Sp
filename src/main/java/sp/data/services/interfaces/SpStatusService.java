0bdb31db1a1326e80aaa479a0dc9f5a2ba462cae
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 01:54:30 +0300

    Initial commit.

---
+++ src/main/java/sp/data/services/interfaces/SpStatusService.java
@@ -0,0 +1,10 @@
+package sp.data.services.interfaces;
+
+
+import sp.data.entities.SpStatus;
+import sp.data.services.generic.GenericService;
+
+public interface SpStatusService extends GenericService<SpStatus, Integer> {
+
+	
+}

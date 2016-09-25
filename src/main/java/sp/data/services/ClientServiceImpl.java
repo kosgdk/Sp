c20a1e4cabd2de0e9640ad0193c303b26336c54b
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 01:49:32 +0300

    Added getByIdWithAllChildren() method implementation.

--- src/main/java/sp/data/services/ClientServiceImpl.java
+++ src/main/java/sp/data/services/ClientServiceImpl.java
@@ -1,12 +1,20 @@
 package sp.data.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sp.data.dao.interfaces.ClientDao;
 import sp.data.entities.Client;
 import sp.data.services.generic.GenericServiceImpl;
 import sp.data.services.interfaces.ClientService;
 
 @Service("ClientService")
 public class ClientServiceImpl extends GenericServiceImpl<Client, Integer> implements ClientService {
-	
 
+    @Autowired
+    ClientDao clientDao;
+
+    @Override
+    public Client getByIdWithAllChildren(int id) {
+        return clientDao.getByIdWithAllChildren(id);
+    }
 }

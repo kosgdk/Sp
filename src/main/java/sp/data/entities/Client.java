476ebdb1bb4c9e996ae0bf806e44277857494c6c
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 01:40:46 +0300

    Added validation annotations. Changed toString() method.

--- src/main/java/sp/data/entities/Client.java
+++ src/main/java/sp/data/entities/Client.java
@@ -1,8 +1,14 @@
 package sp.data.entities;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.internal.Nullable;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -15,10 +21,16 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 @Entity
 @Table(name="clients")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE)
 public class Client {
 
 	@Id
@@ -27,22 +39,30 @@ public class Client {
 	private int id;
 
 	@JsonIgnore
-	@OneToMany(mappedBy="client", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
-	private List<Order> orders;
-	
+	@OneToMany(mappedBy="client", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	private Set<Order> orders;
+
+	@NotNull(message = "{client.name.isEmpty}")
+	@Size(min = 3, max = 50, message = "{client.name.length}")
 	@Column(name="name")
+	@JsonProperty
 	private String name;
-	
+
+	@Size(min = 2, max = 50, message = "{client.realName.length}")
 	@Column(name="real_name")
 	private String realName;
-	
+
+	@Pattern(regexp = "\\+\\d\\(\\d{3}\\)\\d{3}\\-\\d{2}\\-\\d{2}", message = "{client.phone.invalid}")
 	@Column(name="phone")
-	private Long phone;
-	
+	private String phone;
+
+	@Size(max = 500, message = "{client.note.length}")
 	@Column(name="note")
 	private String note;
-	
-	@ManyToOne(cascade=CascadeType.PERSIST, fetch=FetchType.EAGER)
+
+	@NotNull(message = "{client.referer.isEmpty}")
+	@JsonIgnore
+	@ManyToOne(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY)
 	@JoinColumn(name="referer", referencedColumnName="id", nullable=false)
 	private Referer referer;
 	
@@ -61,12 +81,12 @@ public class Client {
 	}
 
 
-	public List<Order> getOrders() {
+	public Set<Order> getOrders() {
 		return orders;
 	}
 
 
-	public void setOrders(List<Order> orders) {
+	public void setOrders(Set<Order> orders) {
 		this.orders = orders;
 	}
 
@@ -91,12 +111,12 @@ public class Client {
 	}
 
 
-	public Long getPhone() {
+	public String getPhone() {
 		return phone;
 	}
 
 
-	public void setPhone(Long phone) {
+	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
@@ -123,8 +143,7 @@ public class Client {
 
 	@Override
 	public String toString() {
-		return "Client [id=" + id + ",\n name=" + name + ",\n realName=" + realName + ",\n phone=" + phone
-				+ ",\n note=" + note + ",\n referer=" + referer + "]";
+		return getName();
 	}
 	
 

2b5e8781c73767a8e3fcd3fd9f5e0bdb072f70d8
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 01:53:43 +0300

    Added getByIdWithAllChildren() method.

--- src/main/java/sp/data/services/interfaces/ClientService.java
+++ src/main/java/sp/data/services/interfaces/ClientService.java
@@ -1,7 +1,10 @@
 package sp.data.services.interfaces;
 
+import sp.data.entities.Client;
+import sp.data.services.generic.GenericService;
 
-public interface ClientService {
+public interface ClientService extends GenericService<Client, Integer> {
 
+    Client getByIdWithAllChildren(int id);
 	
 }

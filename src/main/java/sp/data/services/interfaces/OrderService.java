8e889ff18e22cebbf1dc86a90547eba47e58e156
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 01:54:11 +0300

    Initial commit.

---
+++ src/main/java/sp/data/services/interfaces/OrderService.java
@@ -0,0 +1,14 @@
+package sp.data.services.interfaces;
+
+import sp.data.entities.Order;
+import sp.data.entities.OrderPosition;
+import sp.data.services.generic.GenericService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface OrderService extends GenericService<Order, Integer> {
+
+
+	
+}

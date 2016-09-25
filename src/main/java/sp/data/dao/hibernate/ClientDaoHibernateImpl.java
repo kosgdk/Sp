7056625baf37b6fdb6f61ef608625a211c2428f2
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 00:17:08 +0300

    Added getByIdWithAllChildren() implementation. Changed @Repository name.

--- src/main/java/sp/data/dao/hibernate/ClientDaoHibernateImpl.java
+++ src/main/java/sp/data/dao/hibernate/ClientDaoHibernateImpl.java
@@ -7,11 +7,32 @@ import org.springframework.transaction.annotation.Transactional;
 import sp.data.dao.generic.GenericDaoHibernateImpl;
 import sp.data.dao.interfaces.ClientDao;
 import sp.data.entities.Client;
+import sp.data.entities.Order;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 
 
-@Repository("ClientDaoHibernateImpl")
+@Repository("ClientDao")
 @Transactional(propagation=Propagation.REQUIRED)
 public class ClientDaoHibernateImpl extends GenericDaoHibernateImpl<Client, Integer> implements ClientDao{
 
+    @Override
+    public Client getByIdWithAllChildren(int id) {
 
+        CriteriaBuilder cb = currentSession().getCriteriaBuilder();
+        CriteriaQuery<Client> q = cb.createQuery(Client.class);
+        Root<Client> root = q.from(Client.class);
+
+        Fetch<Client, Order> ClientOrdedFetch = root.fetch("orders");
+        ClientOrdedFetch.fetch("orderPositions", JoinType.LEFT).fetch("product", JoinType.LEFT);
+        ClientOrdedFetch.fetch("sp", JoinType.LEFT);
+
+        q.select(root);
+        q.where(cb.equal(root.<String>get("id"), id));
+
+        TypedQuery<Client> tq = currentSession().createQuery(q);
+
+        return tq.getResultList().get(0);
+    }
 }

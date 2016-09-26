38abd654a4df4a5c3a031323182619046b92a5f7
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 16:41:52 +0300

    Added getProperties() implementation.

--- src/main/java/sp/data/services/PropertiesServiceImpl.java
+++ src/main/java/sp/data/services/PropertiesServiceImpl.java
@@ -1,6 +1,8 @@
 package sp.data.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sp.data.dao.interfaces.PropertiesDao;
 import sp.data.entities.Properties;
 import sp.data.services.generic.GenericServiceImpl;
 import sp.data.services.interfaces.PropertiesService;
@@ -8,5 +10,12 @@ import sp.data.services.interfaces.PropertiesService;
 @Service("PropertiesService")
 public class PropertiesServiceImpl extends GenericServiceImpl<Properties, Integer> implements PropertiesService {
 
+	@Autowired
+	PropertiesDao propertiesDao;
+
+	@Override
+	public Properties getProperties() {
+		return propertiesDao.getById(1);
+	}
 
 }

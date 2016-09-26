bdf7d9affde75e81cbede416969e5d352cd59431
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 16:33:29 +0300

    Initial commit.

---
+++ src/main/java/sp/data/services/interfaces/PropertiesService.java
@@ -0,0 +1,8 @@
+package sp.data.services.interfaces;
+
+import sp.data.entities.Properties;
+import sp.data.services.generic.GenericService;
+
+public interface PropertiesService extends GenericService<Properties, Integer> {
+	
+}

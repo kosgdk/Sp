dfc30f55a934314bafbaf2054078f0e451cdc1c8
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-09-26 15:42:16 +0300

    Initial commit.

---
+++ src/main/java/sp/data/dao/interfaces/PropertiesDao.java
@@ -0,0 +1,8 @@
+package sp.data.dao.interfaces;
+
+import sp.data.dao.generic.GenericDao;
+import sp.data.entities.Properties;
+
+public interface PropertiesDao extends GenericDao<Properties, Integer>{
+
+}

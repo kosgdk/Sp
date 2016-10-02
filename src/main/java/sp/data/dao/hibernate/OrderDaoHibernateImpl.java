cf39c36aa1be66eac09ed6c7ef371d5da5630839
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-02 20:26:39 +0300

    Added getByIdWithAllChildren() method implementation.

--- src/main/java/sp/data/dao/hibernate/OrderDaoHibernateImpl.java
+++ src/main/java/sp/data/dao/hibernate/OrderDaoHibernateImpl.java
@@ -8,11 +8,33 @@ import sp.data.dao.generic.GenericDaoHibernateImpl;
 import sp.data.dao.interfaces.OrderDao;
 import sp.data.entities.Order;
 
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
 @Repository("OrderDaoHibernateImpl")
 @Transactional(propagation=Propagation.REQUIRED)
 public class OrderDaoHibernateImpl extends GenericDaoHibernateImpl<Order, Integer> implements OrderDao{
 
-	//TODO: Add specific methods for Referer
+    @Override
+    public Order getByIdWithAllChildren(int id) {
+
+        String hql = "from Order o " +
+                "left join fetch o.client " +
+                "left join fetch o.sp " +
+                "left join fetch o.orderStatus " +
+                "left join fetch o.place " +
+                "left join fetch o.orderPositions op " +
+                "left join fetch op.product where o.id = :id";
+
+        TypedQuery<Order> query = currentSession().createQuery(hql, Order.class);
+        query.setParameter("id", id);
+
+        try {
+            return  query.getSingleResult();
+        } catch (NoResultException e){
+            return null;
+        }
+    }
 
 
 }

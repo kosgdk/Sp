c5715d5a7c5bbaa1595361e663b9839ed27d5418
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-02 20:27:25 +0300

    Added getByIdWithAllChildren() method implementation.

--- src/main/java/sp/data/services/OrderServiceImpl.java
+++ src/main/java/sp/data/services/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package sp.data.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sp.data.dao.interfaces.OrderDao;
 import sp.data.entities.Order;
 import sp.data.entities.OrderPosition;
 import sp.data.services.generic.GenericServiceImpl;
@@ -12,5 +14,11 @@ import java.util.List;
 @Service("OrderService")
 public class OrderServiceImpl extends GenericServiceImpl<Order, Integer> implements OrderService {
 
+    @Autowired
+    OrderDao orderDao;
 
+    @Override
+    public Order getByIdWithAllChildren(int id) {
+        return orderDao.getByIdWithAllChildren(id);
+    }
 }

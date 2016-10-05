ffaf44faa69cfdc2dbdf2c8c72456317b6c65381
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-05 22:56:32 +0300

    Changed getByIdWithAllChildren() method: CriteriaBuilder replaced with HQL query.

--- src/main/java/sp/data/dao/hibernate/ClientDaoHibernateImpl.java
+++ src/main/java/sp/data/dao/hibernate/ClientDaoHibernateImpl.java
@@ -7,10 +7,9 @@ import org.springframework.transaction.annotation.Transactional;
 import sp.data.dao.generic.GenericDaoHibernateImpl;
 import sp.data.dao.interfaces.ClientDao;
 import sp.data.entities.Client;
-import sp.data.entities.Order;
 
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
 
 
 @Repository("ClientDaoHibernateImpl")
@@ -20,19 +19,20 @@ public class ClientDaoHibernateImpl extends GenericDaoHibernateImpl<Client, Inte
     @Override
     public Client getByIdWithAllChildren(int id) {
 
-        CriteriaBuilder cb = currentSession().getCriteriaBuilder();
-        CriteriaQuery<Client> q = cb.createQuery(Client.class);
-        Root<Client> root = q.from(Client.class);
+        String hql = "from Client c " +
+                        "left join fetch c.orders o " +
+                        "left join fetch o.sp " +
+                        "left join fetch o.orderPositions op " +
+                        "left join fetch op.product where c.id = :id";
 
-        Fetch<Client, Order> ClientOrderFetch = root.fetch("orders");
-        ClientOrderFetch.fetch("orderPositions", JoinType.LEFT).fetch("product", JoinType.LEFT);
-        ClientOrderFetch.fetch("sp", JoinType.LEFT);
+        TypedQuery<Client> query = currentSession().createQuery(hql, Client.class);
+        query.setParameter("id", id);
 
-        q.select(root);
-        q.where(cb.equal(root.<String>get("id"), id));
-
-        TypedQuery<Client> tq = currentSession().createQuery(q);
-
-        return tq.getResultList().get(0);
+        try {
+            return  query.getSingleResult();
+        } catch (NoResultException e){
+            return null;
+        }
     }
+
 }

6b368495dc069485e97cf6825e2282259da11799
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-05 12:02:09 +0300

    Properties and all OrderStatus set as @SessionAttributes. Added createOrderPosition() method.

--- src/main/java/sp/controllers/OrderController.java
+++ src/main/java/sp/controllers/OrderController.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import sp.data.entities.*;
 import sp.data.services.interfaces.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@SessionAttributes("properties")
+@SessionAttributes(value = {"properties", "orderStatuses"})
 public class OrderController {
 
 	@Autowired
@@ -22,6 +24,11 @@ public class OrderController {
 	@ModelAttribute("properties")
 	public Properties getProperties(){
 		return propertiesService.getProperties();
+	}
+
+	@ModelAttribute("orderStatuses")
+	public List<OrderStatus> getOrderStatusesForSelect(){
+		return orderStatusService.getAll();
 	}
 
 	@Autowired
@@ -36,6 +43,12 @@ public class OrderController {
 	@Autowired
 	ProductService productService;
 
+	@Autowired
+	OrderStatusService orderStatusService;
+
+	@Autowired
+	OrderPositionService orderPositionService;
+
 	@InitBinder
 	protected void initBinder(WebDataBinder binder) {
 		binder.setValidator(validator);
@@ -43,21 +56,38 @@ public class OrderController {
 	}
 
 
-	@RequestMapping("/order/{orderId}")
+	// Переход на страницу заказа
+	@RequestMapping(value = "/order/{orderId}", method = RequestMethod.GET)
 	public String orderPage(@PathVariable("orderId") Integer orderId,
 							@ModelAttribute("orderPosition") OrderPosition orderPosition,
 							Model model){
 
-		Order order = orderService.getById(orderId);
+		Order order = orderService.getByIdWithAllChildren(orderId);
 		model.addAttribute("order", order);
+
 		return "order";
 	}
 
-	// Autocomplete ������ ��������
+
+	// Autocomplete выбора продукта
 	@RequestMapping(value = "/getProducts", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Product> getProducts(@RequestParam("query") String productName) {
 		return productService.searchByName(productName);
 	}
 
+	// Обработка запроса на добавление позиции
+	@RequestMapping(value = "/order/{orderId}", method = RequestMethod.POST)
+	public String createOrderPosition(@Valid @ModelAttribute OrderPosition orderPosition,
+									  Errors errors, Model model){
+
+		if (errors.hasErrors()){
+			Order order = orderService.getById(orderPosition.getOrder().getId());
+			model.addAttribute("order", order);
+			return "order";
+		}
+
+		orderPositionService.save(orderPosition);
+		return "redirect:/order/" + orderPosition.getOrder().getId();
+	}
 }

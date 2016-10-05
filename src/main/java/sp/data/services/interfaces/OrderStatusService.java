aef090a45e8ea03e25c3243a79fcb34a584e1666
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-05 11:47:26 +0300

    Added getAllForSelect() method.

--- src/main/java/sp/data/services/interfaces/OrderStatusService.java
+++ src/main/java/sp/data/services/interfaces/OrderStatusService.java
@@ -3,8 +3,10 @@ package sp.data.services.interfaces;
 import sp.data.entities.OrderStatus;
 import sp.data.services.generic.GenericService;
 
+import java.util.Map;
+
 public interface OrderStatusService extends GenericService<OrderStatus, Integer> {
 
-
+	Map<String, String> getAllForSelect();
 	
 }

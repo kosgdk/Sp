95ba6c354b4567bfd14203d09cfe1ecda4a1e04a
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-05 11:48:42 +0300

    Initial commit.

---
+++ src/main/java/sp/data/converters/IdToOrderConverter.java
@@ -0,0 +1,18 @@
+package sp.data.converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import sp.data.entities.Order;
+import sp.data.services.interfaces.OrderService;
+
+
+public class IdToOrderConverter implements Converter<String, Order> {
+
+	@Autowired
+	OrderService orderService;
+
+	@Override
+	public Order convert(String id) {
+		return orderService.getById(Integer.parseInt(id));
+	}
+}

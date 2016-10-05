e15da50caa5f2e98435aecf579411a7afb56b056
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-05 11:51:35 +0300

    Renamed to StringToOrderStatusConverter. Now can convert both Id or Name to OrderStatus.

--- src/main/java/sp/data/converters/StringToOrderStatusConverter.java
+++ src/main/java/sp/data/converters/StringToOrderStatusConverter.java
@@ -3,19 +3,20 @@ package sp.data.converters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import sp.data.entities.OrderStatus;
-import sp.data.entities.Referer;
-import sp.data.services.RefererServiceImpl;
 import sp.data.services.interfaces.OrderStatusService;
 
 
-public class IdToOrderStatusConverter implements Converter<String, OrderStatus> {
+public class StringToOrderStatusConverter implements Converter<String, OrderStatus> {
 
 	@Autowired
 	OrderStatusService orderStatusService;
 
 	@Override
-	public OrderStatus convert(String id) {
-		System.out.println("Inside IdToOrderStatusConverter");
-		return orderStatusService.getById(Integer.parseInt(id));
+	public OrderStatus convert(String string) {
+		if(string.matches("\\d*")){
+			return orderStatusService.getById(Integer.parseInt(string));
+		}else {
+			return orderStatusService.getByName(string);
+		}
 	}
 }

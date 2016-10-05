ae3f3a363031d81b1d43504fe26e2b83f669167c
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-05 17:01:11 +0300

    List of SpStatuses stored as @SessionAttributes.

--- src/main/java/sp/controllers/SpController.java
+++ src/main/java/sp/controllers/SpController.java
@@ -4,18 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import sp.data.entities.*;
+import sp.data.entities.enumerators.SpStatus;
 import sp.data.services.interfaces.*;
 
 import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
+@SessionAttributes(value = {"spStatuses"})
 public class SpController {
+
+	@ModelAttribute("spStatuses")
+	public List<SpStatus> getSpStatusesEnum(){
+		return Arrays.asList(SpStatus.values());
+	}
 
 	@Autowired
 	Validator validator;
@@ -89,8 +96,7 @@ public class SpController {
 	@RequestMapping(value = "/getClients", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Client> getClients(@RequestParam("query") String clientName) {
-		List<Client> clients = clientService.searchByName(clientName);
-		return clients;
+		return clientService.searchByName(clientName);
 	}
 
 }

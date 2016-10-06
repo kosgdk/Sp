d9b93c5428ee4c7b8e17441c6b08f354277d0afc
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-06 11:21:08 +0300

    Initial commit.

---
+++ src/main/java/sp/data/converters/IdToOrderPositionConverter.java
@@ -0,0 +1,18 @@
+package sp.data.converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import sp.data.entities.OrderPosition;
+import sp.data.services.interfaces.OrderPositionService;
+
+
+public class IdToOrderPositionConverter implements Converter<String, OrderPosition> {
+
+	@Autowired
+	OrderPositionService orderPositionService;
+
+	@Override
+	public OrderPosition convert(String id) {
+		return orderPositionService.getById(Integer.parseInt(id));
+	}
+}

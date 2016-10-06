f915d337869c40fe4d71788a3e843669eb7d27b2
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-06 11:21:34 +0300

    Initial commit.

---
+++ src/main/java/sp/data/converters/OrderStatusConverter.java
@@ -0,0 +1,20 @@
+package sp.data.converters;
+
+import sp.data.entities.enumerators.OrderStatus;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class OrderStatusConverter implements AttributeConverter<OrderStatus, Integer> {
+
+	@Override
+	public Integer convertToDatabaseColumn(OrderStatus status) {
+		return status == null ? null : status.getId();
+	}
+
+	@Override
+	public OrderStatus convertToEntityAttribute(Integer id) {
+		return id == null ? null : OrderStatus.getById(id);
+	}
+}

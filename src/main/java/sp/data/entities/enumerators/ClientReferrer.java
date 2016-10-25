fd9f7576af0b3ae15553f49bbd6d58d086c20ab4
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-25 14:55:10 +0300

    Initial commit: created ClientReferrer enum instead of Referrer entity.

---
+++ src/main/java/sp/data/entities/enumerators/ClientReferrer.java
@@ -0,0 +1,61 @@
+package sp.data.entities.enumerators;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public enum ClientReferrer {
+	FORUM(1),
+	VK(2),
+	OTHER(3);
+
+	private final int id;
+
+	private String name;
+
+	ClientReferrer(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		name = getById(id).toString();
+		return name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public static ClientReferrer getById(int id) {
+		switch (id) {
+			case 1: return FORUM;
+			case 2: return VK;
+			case 3: return OTHER;
+			default: return null;
+		}
+	}
+
+	public static ClientReferrer getByName(String name){
+		for (ClientReferrer status : ClientReferrer.values()) {
+			if (status.toString().equals(name)) return status;
+		}
+		return null;
+	}
+
+	public static Map<Integer, String> getAllAsMap(){
+		Map<Integer, String> statuses = new LinkedHashMap<>();
+		for (ClientReferrer status : ClientReferrer.values()) {
+			statuses.put(status.getId(), status.toString());
+		}
+		return statuses;
+	}
+
+	@Override
+	public String toString(){
+		switch (id) {
+			case 1: return "Форум";
+			case 2: return "Вконтакте";
+			case 3: return "Прочее";
+			default: return "NULL";
+		}
+	}
+}

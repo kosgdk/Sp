5d1df8e9b75c979ac039b51fb68255dfb61d109d
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-25 15:17:04 +0300

    Initial commit: created ClientReferrerConverter.

---
+++ src/main/java/sp/data/converters/ClientReferrerConverter.java
@@ -0,0 +1,20 @@
+package sp.data.converters;
+
+import sp.data.entities.enumerators.ClientReferrer;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class ClientReferrerConverter implements AttributeConverter<ClientReferrer, Integer> {
+
+	@Override
+	public Integer convertToDatabaseColumn(ClientReferrer clientReferrer) {
+		return clientReferrer == null ? null : clientReferrer.getId();
+	}
+
+	@Override
+	public ClientReferrer convertToEntityAttribute(Integer id) {
+		return id == null ? null : ClientReferrer.getById(id);
+	}
+}

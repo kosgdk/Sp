f9e990729a50f88ecb3ce37ecd4c830e536df785
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-10-25 15:35:38 +0300

    Initial commit: created PlaceConverter.

---
+++ src/main/java/sp/data/converters/PlaceConverter.java
@@ -0,0 +1,20 @@
+package sp.data.converters;
+
+import sp.data.entities.enumerators.Place;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class PlaceConverter implements AttributeConverter<Place, Integer> {
+
+	@Override
+	public Integer convertToDatabaseColumn(Place place) {
+		return place == null ? null : place.getId();
+	}
+
+	@Override
+	public Place convertToEntityAttribute(Integer id) {
+		return id == null ? null : Place.getById(id);
+	}
+}

c2b1436a27bd856e346981aee943b4b8f1de64ae
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-12-21 18:24:22 +0300

    New tests added. Made some refactoring.

--- src/test/java/sp/dbtests/OrderPositionDbIntegrationTest.java
+++ src/test/java/sp/dbtests/OrderPositionDbIntegrationTest.java
@@ -4,7 +4,6 @@ import com.vladmihalcea.sql.SQLStatementCountValidator;
 import net.sf.ehcache.CacheManager;
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.unitils.UnitilsBlockJUnit4ClassRunner;
 import org.unitils.database.annotations.Transactional;
 import org.unitils.database.util.TransactionMode;
@@ -13,7 +12,6 @@ import org.unitils.dbunit.annotation.ExpectedDataSet;
 import org.unitils.spring.annotation.SpringApplicationContext;
 import org.unitils.spring.annotation.SpringBeanByType;
 import sp.data.dao.interfaces.OrderPositionDao;
-import sp.data.dao.interfaces.ProductDao;
 import sp.data.entities.OrderPosition;
 
 import static org.junit.Assert.*;
@@ -29,42 +27,41 @@ public class OrderPositionDbIntegrationTest {
     @SpringBeanByType
     OrderPositionDao orderPositionDao;
 
-    @SpringBeanByType
-    ProductDao productDao;
-
 
     @Before
     public void setUp(){
         SQLStatementCountValidator.reset();
-        CacheManager.getInstance().clearAll(); //Clearing 2nd level cache
+        CacheManager.getInstance().clearAll();
     }
 
+    @Test
+    public void getById_ShouldMakeOneQuery(){
+        orderPositionDao.getById(1L);
+        assertSelectCount(1);
+    }
 
     @Test
-    public void getById_testFetchTypeOfChildren(){
+    public void getById_ProductShouldBeLoadedEagerly(){
         OrderPosition orderPosition = orderPositionDao.getById(1L);
-        assertSelectCount(1);
-
         assertNotNull(orderPosition.getProduct());
-        assertSelectCount(1);
         assertEquals(new Long(1), orderPosition.getProduct().getId());
-        assertSelectCount(1); // Test for EAGER loading of Product
-
-        assertNotNull(orderPosition.getOrder());
         assertSelectCount(1);
-        System.out.println(orderPosition.getOrder().getId());
+    }
+
+    @Test
+    public void getById_OrderShouldBeLoadedLazily(){
+        OrderPosition orderPosition = orderPositionDao.getById(1L);
+        assertNotNull(orderPosition.getOrder());
         assertEquals(new Long(1), orderPosition.getOrder().getId());
-        assertSelectCount(2); // Test for LAZY loading of Order
+        assertSelectCount(2);
     }
 
     @Test
     @ExpectedDataSet("unitils/dataset/integration/orderPosition/update_ShouldNotCascadeUpdateChild_Expected.xml")
     public void update_ShouldNotCascadeUpdateChild(){
         OrderPosition orderPosition = orderPositionDao.getById(1L);
-        assertSelectCount(1);
         orderPosition.getProduct().setName("TestProduct");
         orderPosition.getOrder().setNote("TestNote");
-        assertSelectCount(2);
         orderPosition.setNote("TestNote");
         orderPositionDao.update(orderPosition);
         assertUpdateCount(1);
@@ -76,11 +73,6 @@ public class OrderPositionDbIntegrationTest {
         orderPositionDao.deleteById(1L);
         assertSelectCount(1);
         assertDeleteCount(1);
-    }
-
-    @Test(expected = DataIntegrityViolationException.class)
-    public void delete_ChildProduct_ShouldCauseException(){
-        productDao.deleteById(1L);
     }
 
 }
